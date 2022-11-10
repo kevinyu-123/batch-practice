8e42d1a983e53f80215b30ee058c2476df3f6058
hj <tntj123@naver.com>
2022-11-10 23:42:59 +0900

    start product feature branch

--- src/main/java/com/practice/qtrproject/controller/ProductController.java
+++ src/main/java/com/practice/qtrproject/controller/ProductController.java
@@ -1,29 +1,16 @@
 package com.practice.qtrproject.controller;
 
-import com.practice.qtrproject.service.ApiService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
-@Api("test 컨트롤러")
-@RequestMapping("/api/v1")
+@Api("상품")
+@RequestMapping("/api/v1/product")
 @RestController
-public class ApiController {
-
-    private final ApiService service;
-
-    @GetMapping("/test")
-    public String testApi(){
-        return "connection test";
-    }
-
-
-
-
+public class ProductController {
 
 }
