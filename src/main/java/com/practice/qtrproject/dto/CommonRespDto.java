829b690f388a2d85fd39d7a66070ba9cfea1359e
hj <tntj123@naver.com>
2022-11-14 23:47:25 +0900

    exception handler 추가

---
+++ src/main/java/com/practice/qtrproject/dto/CommonRespDto.java
@@ -0,0 +1,22 @@
+package com.practice.qtrproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonRespDto<T> {
+
+    private Integer code; // 1:성공, 2:실패
+
+    private String msg; //에러 또는 성공 메세지 제공
+
+    private T body;
+
+
+}
+
---
+++ src/main/java/com/practice/qtrproject/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.practice.qtrproject.handler;
+
+import com.practice.qtrproject.dto.CommonRespDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> apiException(RuntimeException e){
+        return new ResponseEntity<>(CommonRespDto.builder().code(2).msg(e.getMessage()).build(), HttpStatus.BAD_REQUEST);
+    }
+}
