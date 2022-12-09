474acf9447d6659846c46a45736c6fd317d235eb
hj <tntj123@naver.com>
2022-12-09 21:32:06 +0900

    batch begin

--- src/main/java/com/practice/qtrproject/QuarterProjectApplication.java
+++ src/main/java/com/practice/qtrproject/QuarterProjectApplication.java
@@ -1,11 +1,15 @@
 package com.practice.qtrproject;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableBatchProcessing
+@EnableScheduling
 public class QuarterProjectApplication {
 
     public static void main(String[] args) {
