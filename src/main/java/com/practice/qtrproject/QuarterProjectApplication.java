006db3e2c61e35633615a609508136798c76f15d
hj <tntj123@naver.com>
2023-01-02 22:44:58 +0900

    configuring

--- src/main/java/com/practice/qtrproject/QuarterProjectApplication.java
+++ src/main/java/com/practice/qtrproject/QuarterProjectApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableBatchProcessing
 @EnableScheduling
 public class QuarterProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(QuarterProjectApplication.class, args);
     }
--- src/main/java/com/practice/qtrproject/batch/config/CustomBatchConfigurer.java
+++ src/main/java/com/practice/qtrproject/batch/config/CustomBatchConfigurer.java
@@ -2,6 +2,7 @@ package com.practice.qtrproject.batch.config;
 
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -10,8 +11,10 @@ import javax.sql.DataSource;
 @EnableBatchProcessing
 public class CustomBatchConfigurer extends DefaultBatchConfigurer {
 
+    @Autowired
+    private DataSource dataSource;
     @Override
     public void setDataSource(DataSource dataSource) {
-        // 여기를 비워놓는다
+        this.dataSource = dataSource;
     }
 }
