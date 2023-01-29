c979e47f8943c089f1cd3dea4fe4ca48c05a40b0
hj <tntj123@naver.com>
2023-01-29 18:17:07 +0900

    상세검색 값 추가

--- src/main/java/com/practice/qtrproject/dto/request/SearchParamDto.java
+++ src/main/java/com/practice/qtrproject/dto/request/SearchParamDto.java
@@ -4,13 +4,9 @@ import lombok.Data;
 
 @Data
 public class SearchParamDto {
-
     private String regDate;
-
     private String updDate;
-
     private String delDate;
-
     private String searchType;
     private String value;
 
